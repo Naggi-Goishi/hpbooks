bafa813372d1c8cad1993d81c0676f0c494913f9
Naggi-Goishi <naggitapdance@gmail.com>
2017-02-02 08:08:50 -0800

    add task of creating database in setup.rb

--- bin/setup.rb
+++ bin/setup.rb
@@ -1,8 +1,10 @@
 require_relative 'correct_ruby_version'
 require_relative 'bundle_install'
+require_relative 'db/create'
 
 check_ruby_version
 bundle_install
+create_database
 
 puts "\n\nYou are all set!"
 puts "Start your server with 'ruby hpbooks.rb' and open localhost:4567!!\n\n"
