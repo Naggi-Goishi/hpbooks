ee715de193cc86048e35718bdfeb671eaaccb945
Naggi-Goishi <naggitapdance@gmail.com>
2017-02-02 08:12:13 -0800

    add sign up path

--- hpbooks.rb
+++ hpbooks.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
 require 'slim'
+require 'json'
 
 get '/' do
+  slim :index
+end
+
+post '/sign_up' do
+  load('controllers/UserController.rb')
+  controller = UserController.new
+
+  controller.save(params)
   slim :index
 end
